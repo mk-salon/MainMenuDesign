d801dab4ea2411dca24ac0d7657a04e6dbbf579c
Andrii Malnov <andrey.malnov@gmail.com>
2021-08-05 18:11:46 +0300

    Initial Commit

---
+++ MainMenuDesign/MainMenuDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainMenuDesignApp.swift
+//  MainMenuDesign
+//
+//  Created by Mac on 05.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MainMenuDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
